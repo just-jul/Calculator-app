aa93daf51599e1649fbbb99d4ebe1d6caa8ff456
just-jul <businessbyjul@gmail.com>
2025-10-08 09:28:53 +0100

    Make the display and buttons functional, create logic that prevents the operators to be displayed more than once per operation

--- main.js
+++ main.js
@@ -31,6 +31,11 @@ const substrBtn = document.querySelector(".substr");
 const multBtn = document.querySelector(".multiply");
 const divideBtn = document.querySelector(".divide");
 const equalBtn = document.querySelector(".equal");
+const clearBtn = document.querySelector(".clear");
+const resultText = document.querySelector(".result-text");
+
+const numberBtn = document.querySelectorAll(".number");
+const btn = document.querySelectorAll(".btn");
 
 const seven = document.querySelector(".seven");
 const eight = document.querySelector(".eight");
@@ -41,3 +46,33 @@ const six = document.querySelector(".six");
 const one = document.querySelector(".one");
 const two = document.querySelector(".two");
 const three = document.querySelector(".three");
+
+let operatorUsed = false;
+
+numberBtn.forEach(btn => {
+    btn.onclick = () => {
+        resultText.textContent += btn.textContent;
+        if (resultText.textContent.length > 30) {
+            alert("Number too big!");
+            resultText.textContent = "";
+        }
+    }
+});
+
+function handleOperator (op){
+    if (!operatorUsed && resultText.textContent !== ''){
+        resultText.textContent += op;
+        operatorUsed = true;
+    }else{
+        console.log("Operator already used.");
+    }
+}
+
+addBtn.onlick = () => handleOperator('+');
+substrBtn.onlick = () => handleOperator('-');
+multBtn.onclick = () => handleOperator('*');
+divideBtn.onlick = () => handleOperator('/');
+
+clearBtn.onclick = () => {
+    resultText.textContent = '';
+}
